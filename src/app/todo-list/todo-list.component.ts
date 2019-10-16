3015b929244d76d2a643cede7fbcfdd297409bdc
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2019-10-16 21:22:39 +0530

    add jquery and basic layout

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
+  preserveWhitespaces: true
 })
 export class TodoListComponent implements OnInit {
 
